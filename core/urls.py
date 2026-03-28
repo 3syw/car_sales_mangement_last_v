01c468c33c9ff66743fd68f828d84f397c96298e
ComputerWorld <computerworld@local.dev>
2026-03-28 21:05:28 +0300

    Fix Render health check timeout with dedicated /healthz endpoint

--- core/urls.py
+++ core/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 from sales.views import home, dashboard, export_sales_excel, export_financial_excel, export_timeline_excel, financial_report_charts_data, car_list, car_edit, car_detail, process_sale, financial_reports, car_profit_report, showroom_performance_report, inventory_turnover_report, stale_cars_report, audit_logs_report, export_audit_logs_excel, export_audit_logs_csv, financial_reports_detail, vouchers_reports, vouchers_list, receipt_voucher, payment_voucher, operating_expenses_voucher, general_expenses_management, digital_archive, inventory_reconciliation, debts_list, debt_aging_report, add_debt_payment, timeline_view, user_logout, register, user_login, admin_user_filters, system_users, available_cars_table, sold_cars_cards, sold_car_details, permissions_management, delete_system_user, platform_owner_login, platform_switch_tenant, platform_exit_impersonation, central_audit_monitor, daily_closing_control, cash_flow_projection, bank_reconciliation, financial_consistency_checker, chart_of_accounts, trial_balance_report, financial_containers_management, fiscal_period_closing_control, google_auth_start, google_auth_callback
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('healthz/', lambda request: HttpResponse('ok', content_type='text/plain'), name='healthz'),
     path('login/', user_login, name='login'),
     path('admin/login/', user_login, name='admin_login'),
     path('auth/google/start/', google_auth_start, name='google_auth_start'),
